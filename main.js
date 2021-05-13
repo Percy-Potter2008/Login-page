b33169458d0535ca5407a3fa7127f8b7d1fb899a
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-05-13 19:35:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function addUser(){
+function User(){
   player1_name = document.getElementById("player1_name_input").value;
   player2_name = document.getElementById("player2_name_input").value;
   localStorage.setItem("player1_name", player1_name);
